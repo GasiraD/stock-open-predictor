732c53be76d49a1c10e2c72cd9eece692e5b5d3c
GasiraD <vainglory6973@gmail.com>
2024-08-22 07:23:31 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -9,9 +9,8 @@ from math import sqrt
 
 
 
-#하이퍼 파라미터
+#hyperparameter
 company_name = "SAMYANG CORPORATION"
-
 
 num_layers = 1
 size_layer = 32
@@ -22,7 +21,7 @@ batch_size = 16
 train_size = 0.7
 
 
-# 1. 데이터 읽기 및 필터링
+# 데이터 읽기 및 필터링
 df = pd.read_csv('train.csv')
 df = df[df['Company_Name'] == company_name]
 df['Date'] = pd.to_datetime(df['Date'])
@@ -30,21 +29,23 @@ df = df.sort_values(by='Date')
 df.dropna(inplace=True)
 
 
-# 1.5 테스트용 임시 수정
+# 테스트용 임시 수정
 # df = df.head(365)
 
 
-# 2. 데이터 선택 및 정규화
+# 데이터 선택 및 정규화
 open = df['Open'].values.reshape(-1, 1)
 scaler = MinMaxScaler(feature_range=(0, 1))
 open_scaled = scaler.fit_transform(open)
 
 
 
-# 3. 테스트 데이터 분할
+# 테스트 데이터 분할
 train_size = int(len(open_scaled) * train_size)
 train_data, test_data = open_scaled[:train_size], open_scaled[train_size:]
 
+
+# 시계열 데이터 준비
 
 def create_dataset(data, time_step=1):
     X, y = [], []
@@ -53,17 +54,16 @@ def create_dataset(data, time_step=1):
         y.append(data[i + time_step, 0])
     return np.array(X), np.array(y)
 
-# 5. 시계열 데이터 준비
 X_train, y_train = create_dataset(train_data, time_step)
 X_test, y_test = create_dataset(test_data, time_step)
 
 
-# 6. 데이터 형태 변경 (LSTM 입력 형태에 맞게)
+# 데이터 형태 변경 (LSTM)
 X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
 X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
 
 
-# 7. LSTM 모델 정의
+# LSTM 모델 정의
 
 model = tf.keras.models.Sequential()
 for _ in range(num_layers):
@@ -78,13 +78,13 @@ model.compile(optimizer='adam', loss='mean_squared_error')
 
 history = model.fit(X_train, y_train, epochs=epoch, batch_size=batch_size, validation_split=0.1)
 
-# 9. 모델 평가
+# 모델 평가
 
-# 9.1 loss 평가
+## loss 평가
 loss = model.evaluate(X_test, y_test)
 print(f'Test loss: {loss}')
 
-# 9.2 rmse 평가
+## rmse 평가
 train_predictions = model.predict(X_train)
 test_predictions = model.predict(X_test)
 train_predictions = scaler.inverse_transform(train_predictions)
@@ -92,13 +92,12 @@ y_train_unscaled = scaler.inverse_transform(y_train.reshape(-1, 1)).flatten()
 test_predictions = scaler.inverse_transform(test_predictions)
 y_test_unscaled = scaler.inverse_transform(y_test.reshape(-1, 1)).flatten()
 
-# RMSE 계산 함수
 def calculate_rmse(y_true, y_pred):
     mse = mean_squared_error(y_true, y_pred)
     rmse = sqrt(mse)
     return rmse
 
-# RMSE 계산
+
 train_rmse = calculate_rmse(y_train_unscaled, train_predictions.flatten())
 test_rmse = calculate_rmse(y_test_unscaled, test_predictions.flatten())
 
@@ -106,7 +105,7 @@ print(f'Training RMSE: {train_rmse:.4f}')
 print(f'Testing RMSE: {test_rmse:.4f}')
 
 
-# 10. 예측 및 결과 시각화
+# 예측 및 결과 시각화
 predictions = model.predict(X_test)
 predictions = scaler.inverse_transform(predictions)
 y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
